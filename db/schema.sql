0b225e41d422362a124d5866a6c26ec25dbd88a2
Jimbei <contrerasr2272@gmail.com>
2022-08-16 13:23:13 -0400

    Adding seeds and schemas

---
+++ db/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Tiffany', 'Contreras', 1, 1),
+('Jacob', 'Lee', 2, NULL),
+('Sarah', 'Glee', 1, 2),
+('Ryan', 'Connor', 1, 1),
+('Sierra', 'Summers', 3, 3);
