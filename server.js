93b484e107260657b340a21330db69b85c017c7c
Jimbei <contrerasr2272@gmail.com>
2022-08-17 12:09:31 -0400

    db.query selecting from employee

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require('express');
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 const app = express();
+const mysql = require('mysql2');
 
 //Express Middleware
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+//Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //your MYSQL username,
+        user: 'root',
+        //Your MYSQL password
+        password: 'BootCamp217!',
+        database: 'tracker'
+    },
+    console.log('Connected to the tracker database.')
+);
+
+db.query(`SELECT * FROM employee`, (err, rows) => {
+    console.log(rows);
+});
 
 
 //Default response for any other request (Not Found)
@@ -21,5 +39,5 @@ app.use((req, res) => {
 
 
 app.listen(PORT, () => {
-    console.log(`Server runnig on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
